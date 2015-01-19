17ec863b855fe250fcea1e0fcbbd97ac4648f115
lilianascortea01 <liliana.scortea@yahoo.com>
2015-01-19 23:29:32 +0200

    Create README.py

---
+++ README.py
@@ -0,0 +1,6 @@
+echo # azuretest >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/andreeatocanie/azuretest.git
+git push -u origin master
